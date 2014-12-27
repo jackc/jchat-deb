5f1a569e06ca443c119ad9237d4285734fe94609
Jack Christensen <jack@jackchristensen.com>
2014-12-27 15:20:45 -0600

    Enter necessary package config

--- debian/jchat.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the jchat package
-#
-0 4	* * *	root	[ -x /usr/bin/jchat_maintenance ] && /usr/bin/jchat_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(jchat):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="jchat" command="/usr/bin/jchat"
